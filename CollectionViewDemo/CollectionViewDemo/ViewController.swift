87894bc12bf8d3c2d3fcb32d5fe99a6827892517
Thiha Aung <leothihaaung@gmail.com>
2019-02-13 15:54:21 +0900

    raw animation done

--- CollectionViewDemo/CollectionViewDemo/ViewController.swift
+++ CollectionViewDemo/CollectionViewDemo/ViewController.swift
@@ -8,9 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+        selectedCell = collectionView.cellForItem(at: indexPath)!
+        let detailViewController = storyboard!.instantiateViewController(withIdentifier: "detailViewController")
+        detailViewController.transitioningDelegate = self
+        
+        present(detailViewController, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    // Flow Layout
+    
+    //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+    //        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 2)
+    //    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let collectionViewWidth = collectionView.bounds.width - 2 * 5
+        return CGSize(width: (collectionViewWidth/3), height: (collectionViewWidth/3))
+    }
+    
+    //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+    //        return 2
+    //    }
+    
+    //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    //        return 0
+    //    }
+}
+
+class ViewController: UIViewController, UICollectionViewDataSource {
     
     @IBOutlet weak var myCollectionView: UICollectionView!
+    
+    let transition = TransitionAnimator()
+    var selectedCell = UICollectionViewCell()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +54,10 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         
         myCollectionView.dataSource = self
         myCollectionView.delegate = self
+        
+        transition.dismissCompletion = {
+            self.selectedCell.isHidden = false
+        }
     }
 
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -34,26 +74,21 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         
         return myCell
     }
-    
-    
-    // Flow Layout
-    
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-//        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 2)
-//    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let collectionViewWidth = collectionView.bounds.width - 10
-        return CGSize(width: (collectionViewWidth/3), height: (collectionViewWidth/3))
-    }
-
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-//        return 2
-//    }
-
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-//        return 0
-//    }
-
 }
 
+extension ViewController: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        guard let originFrame = selectedCell.superview?.convert(selectedCell.frame, to: nil) else {
+            return transition
+        }
+        transition.originFrame = originFrame
+        transition.presenting = true
+        selectedCell.isHidden = true
+        return transition
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.presenting = false
+        return transition
+    }
+}
