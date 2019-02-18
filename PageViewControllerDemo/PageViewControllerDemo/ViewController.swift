c1c11f36ee09d456e6c554163bb8133a7a068085
Thiha Aung <leothihaaung@gmail.com>
2019-02-18 14:06:57 +0900

    page view controller initial

---
+++ PageViewControllerDemo/PageViewControllerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PageViewControllerDemo
+//
+//  Created by Thiha Aung on 2019/02/18.
+//  Copyright © 2019 Thiha Aung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
